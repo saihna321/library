dea6eed76b9215f4285315ec76a6f5128aabe6af
saikhna <saikhanjargal.0321@gmail.com>
2024-10-30 18:35:45 +0800

    first

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './styles/App.css';
+import Header from './components/Header';
+import SearchResults from './components/SearchResuts';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <SearchResults></SearchResults>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { faSearch, faBookBookmark} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+function Header() {
+    return (
+        <div className='header'>
+            <h1 className='logo'>NomiinSan</h1>
+            <SearchBar></SearchBar>
+            <SavedBooks></SavedBooks>
+        </div>
+        
+    )
+}
+
+function SearchBar() {
+    return (
+        <div className='searchbar'>
+            <div className="search-container">
+                <FontAwesomeIcon icon={faSearch} className="search-icon" />
+                <input placeholder='ном хайх...' className='search-input' />
+            </div>
+            <button className='search-button'>Хайх</button>
+        </div>
+    )
+}
+
+function SavedBooks() {
+    let saved = 3
+    return (
+        <div className='saved-books'>
+            <button className='save-icon'>            <FontAwesomeIcon icon={faBookBookmark}/>
+            </button>
+            <div className='saved-book-number'>
+                {saved}
+            </div>
+        </div>
+    )
+}
+
+export default Header
